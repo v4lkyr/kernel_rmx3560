35d0c97b5917cc36c35c4bcb250dc8523f068074
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-12-01 13:09:06 +0100

    sched: Fix kernel-doc markup
    Kernel-doc requires that a kernel-doc markup to be immediately
    below the function prototype, as otherwise it will rename it.
    So, move sys_sched_yield() markup to the right place.
    
    Also fix the cpu_util() markup: Kernel-doc markups
    should use this format:
            identifier - description
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/50cd6f460aeb872ebe518a8e9cfffda2df8bdb0a.1606823973.git.mchehab+huawei@kernel.org
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6435,14 +6435,6 @@ SYSCALL_DEFINE3(sched_getaffinity, pid_t, pid, unsigned int, len,
 	return ret;
 }
 
-/**
- * sys_sched_yield - yield the current processor to other threads.
- *
- * This function yields the current CPU to other tasks. If there are no
- * other threads running on this CPU then this function will return.
- *
- * Return: 0.
- */
 static void do_sched_yield(void)
 {
 	struct rq_flags rf;
@@ -6467,6 +6459,14 @@ static void do_sched_yield(void)
 	schedule();
 }
 
+/**
+ * sys_sched_yield - yield the current processor to other threads.
+ *
+ * This function yields the current CPU to other tasks. If there are no
+ * other threads running on this CPU then this function will return.
+ *
+ * Return: 0.
+ */
 SYSCALL_DEFINE0(sched_yield)
 {
 	do_sched_yield();
