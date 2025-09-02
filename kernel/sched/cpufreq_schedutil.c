81c93eb2741e5b413ffa07a9d41031e06e2ef2c2
Sultan Alsawaf <sultan@kerneltoast.com>
2025-09-03 00:35:00 +0800

    cpufreq: schedutil: Allow single-CPU frequency to drop without idling
    Given that a CPU's clock is gated at even the shallowest idle state,
    waiting until a CPU idles at least once before reducing its frequency is
    putting the cart before the horse. For long-running workloads with low
    compute needs, requiring an idle call since the last frequency update to
    lower the CPU's frequency results in significantly increased energy usage.
    
    Given that there is already a mechanism in place to ratelimit frequency
    changes, this heuristic is wholly unnecessary.
    
    Allow single-CPU performance domains to drop their frequency without
    requiring an idle call in between to improve energy. Right off the bat,
    this reduces CPU power consumption by 7.5% playing a cat gif in Firefox on
    a Pixel 8 (270 mW -> 250 mW). And there is no visible loss of performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -59,11 +59,6 @@ struct sugov_cpu {
 
 	unsigned long		util;
 	unsigned long		bw_min;
-
-	/* The field below is for single-CPU policies only: */
-#ifdef CONFIG_NO_HZ_COMMON
-	unsigned long		saved_idle_calls;
-#endif
 };
 
 static DEFINE_PER_CPU(struct sugov_cpu, sugov_cpu);
@@ -498,19 +493,6 @@ static unsigned long sugov_iowait_apply(struct sugov_cpu *sg_cpu, u64 time,
 	return (sg_cpu->iowait_boost * max_cap) >> SCHED_CAPACITY_SHIFT;
 }
 
-#ifdef CONFIG_NO_HZ_COMMON
-static bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu)
-{
-	unsigned long idle_calls = tick_nohz_get_idle_calls_cpu(sg_cpu->cpu);
-	bool ret = idle_calls == sg_cpu->saved_idle_calls;
-
-	sg_cpu->saved_idle_calls = idle_calls;
-	return ret;
-}
-#else
-static inline bool sugov_cpu_is_busy(struct sugov_cpu *sg_cpu) { return false; }
-#endif /* CONFIG_NO_HZ_COMMON */
-
 /*
  * Make sugov_should_update_freq() ignore the rate limit when DL
  * has increased the utilization.
@@ -526,7 +508,6 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 {
 	struct sugov_cpu *sg_cpu = container_of(hook, struct sugov_cpu, update_util);
 	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
-	unsigned int cached_freq = sg_policy->cached_raw_freq;
 	unsigned long max_cap;
 	unsigned int next_f;
 	unsigned long boost;
@@ -545,16 +526,6 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	sugov_get_util(sg_cpu, boost);
 
 	next_f = get_next_freq(sg_policy, sg_cpu->util, max_cap);
-	/*
-	 * Do not reduce the frequency if the CPU has not been idle
-	 * recently, as the reduction is likely to be premature then.
-	 */
-	if (sugov_cpu_is_busy(sg_cpu) && next_f < sg_policy->next_freq) {
-		next_f = sg_policy->next_freq;
-
-		/* Restore cached freq as next_freq has changed */
-		sg_policy->cached_raw_freq = cached_freq;
-	}
 
 	/*
 	 * This code runs under rq->lock for the target CPU, so it won't run
