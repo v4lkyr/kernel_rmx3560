f08034161f746387072bd3f8d3b0c1ab652ecd20
Zhen Lei <thunder.leizhen@huawei.com>
2026-06-10 14:57:03 +0800

    kallsyms: Add helper kallsyms_on_each_match_symbol()
    Function kallsyms_on_each_symbol() traverses all symbols and submits each
    symbol to the hook 'fn' for judgment and processing. For some cases, the
    hook actually only handles the matched symbol, such as livepatch.
    
    Because all symbols are currently sorted by name, all the symbols with the
    same name are clustered together. Function kallsyms_lookup_names() gets
    the start and end positions of the set corresponding to the specified
    name. So we can easily and quickly traverse all the matches.
    
    The test results are as follows (twice): (x86)
    kallsyms_on_each_match_symbol:     7454,     7984
    kallsyms_on_each_symbol      : 11733809, 11785803
    
    kallsyms_on_each_match_symbol() consumes only 0.066% of
    kallsyms_on_each_symbol()'s time. In other words, 1523x better
    performance.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>
    Signed-off-by: v4lkyr <valkyr23@gmail.com>

--- include/linux/kallsyms.h
+++ include/linux/kallsyms.h
@@ -80,6 +80,9 @@ int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 				      unsigned long),
 			    void *data);
 
+int kallsyms_on_each_match_symbol(int (*fn)(void *, unsigned long),
+				  const char *name, void *data);
+
 extern int kallsyms_lookup_size_offset(unsigned long addr,
 				  unsigned long *symbolsize,
 				  unsigned long *offset);
@@ -158,6 +161,12 @@ static inline bool kallsyms_show_value(const struct cred *cred)
 
 static inline int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 					  unsigned long), void *data)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int kallsyms_on_each_match_symbol(int (*fn)(void *, unsigned long),
+						const char *name, void *data)
 {
 	return -EOPNOTSUPP;
 }
